8f00c7dcd6de29766dd21a9b4cc26858f4d39300
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-03-19 16:42:56 +0100

    Player Controller

---
+++ Red Zone Quarantine/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed;
+    public float turnSpeed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if(Input.GetKey("up"))
+        {
+            transform.Translate(0, walkSpeed * Time.deltaTime, 0);
+        }
+
+        if(Input.GetKey("down"))
+        {
+            transform.Translate(0, -walkSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(walkSpeed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(-walkSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
