6e9cfbac0cdbc0f226099500d4ab8c33057ffa5b
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-03-28 20:14:26 +0100

    Base Item script

---
+++ Red Zone Quarantine/Assets/Scripts/Items/BaseItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseItem
+{
+    public enum ItemTypes { EQUIPMENT, CONSUMABLE }
+
+    private string itemName;
+    private string itemDescription;
+    private int itemID;
+    private ItemTypes itemType;
+
+    public string ItemName { get { return itemName; } set { itemName = value; } }
+    public string ItemDescription { get { return itemDescription; } set { itemDescription = value; } }
+    public int ItemID { get { return itemID; } set { itemID = value; } }
+    public ItemTypes ItemType { get { return itemType; } set { itemType = value; } }
+}
